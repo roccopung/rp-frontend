8bd38dd971fe4d58420aeb1df8f61bd1c42742ae
Rocco Punghellini <rocco.punghellini@gmail.com>
2024-01-03 12:25:59 +0100

    app update

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script setup></script>
 <template>
   <NuxtLayout>
-    <NuxtPage></NuxtPage>
+    <NuxtPage :transition="{
+      name: 'fade',
+      mode: 'out-in',
+    }" />
+    <NuxtLoadingIndicator color="black" />
   </NuxtLayout>
 </template>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.15s;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}</style>
